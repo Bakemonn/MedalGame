d901b8370322c62a6dd0ef98aa813ea40cc1cb20
Bakemonn <mbakgminc@gmail.com>
2017-06-10 00:26:20 +0900

    Dice

--- Assets/Scripts/Ball.cs
+++
@@ -1,11 +0,0 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class Ball : MonoBehaviour {    public GameObject obj;    public Rigidbody rb;    bool hitBat;	// Use this for initialization	void Start () {        obj = gameObject;        rb = obj.GetComponent<Rigidbody>();        hitBat = false;	}
-
-    // Update is called once per frame
-    void Update() {
-        //ボールがバットに当たるまで力を加える
-        if (hitBat == false) {
-            rb.AddForce(0, 0, -1, ForceMode.Impulse);
-        }    }    void Hit()
-    {
-        this.hitBat = true;
-    }}
